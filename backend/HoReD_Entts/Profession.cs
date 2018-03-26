53f64871ba9aa16578217dba25ee274ff316aa31
usyktt <usyktt@gmail.com>
2018-03-26 15:56:54 +0300

    Added .gitignore

---
+++ backend/HoReD_Entts/Profession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoReD_Entts
+{
+    class Profession
+    {
+        public int ProfessionId { get; set; }
+
+        public string ProfessionName { get; set; }
+
+        public bool IsStatic { get; set; }
+    }
+}
