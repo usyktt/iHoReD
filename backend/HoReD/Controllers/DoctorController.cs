081bdf9494693cf4212330c2ad89c938ea296fa1
Olya <solyarolya@gmail.com>
2018-03-27 15:19:48 +0300

    Fixed bugs with controller

--- backend/HoReD/Controllers/DoctorController.cs
+++ backend/HoReD/Controllers/DoctorController.cs
@@ -1,11 +1,6 @@
 ﻿using HoReD_Entts;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using HoReD_Entts.Services;
 
--- backend/HoReD_Entts/Services/DoctorService.cs
+++ backend/HoReD_Entts/Services/DoctorService.cs
@@ -15,7 +15,6 @@ namespace HoReD_Entts.Services
         public Doctor GetDoctorById(int id)
         {
             var cmd = $"Select * from Doctors where IDDoctors = {id}";
-           // _dbContext.OpenConnection();
             var str = _dbContext.ExecuteSqlQuery(cmd, '*');
             var values = str.Split('*');
             var doctor = new Doctor
@@ -27,8 +26,6 @@ namespace HoReD_Entts.Services
                 EmployingDate = Convert.ToDateTime(values.GetValue(4)),
                 Image = values.GetValue(5).ToString()
             };
-
-
             _dbContext.Dispose();
 
             return doctor;
@@ -36,27 +33,26 @@ namespace HoReD_Entts.Services
 
         public List<DoctorInfo> GetDoctors()
         {
-            var cmd = "exec GETINFOABOUTDOCTORS";
-            //_dbContext.OpenConnection();
+            const string cmd = "GETINFOABOUTDOCTORS";
             var str = _dbContext.ExecuteSqlQuery(cmd, '*');
             var values = str.Split('*');
-            List<DoctorInfo> list = new List<DoctorInfo>();
-            for (int i = 0; i < values.Length/8; i++)
+            var list = new List<DoctorInfo>();
+            for (int i = 0; i < (values.Length-1); i+=8)
             {
                 var doctor = new DoctorInfo
                 {
-                    Id = Convert.ToInt32(values.GetValue(i * 0)),
-                    FirstName = values.GetValue(i * 1).ToString(),
-                    LastName = values.GetValue(i * 2).ToString(),
-                    ProfessionName = values.GetValue(i * 3).ToString(),
-                    StartHour = TimeSpan.Parse(values.GetValue(i * 4).ToString()),
-                    EndHour = TimeSpan.Parse(values.GetValue(i * 5).ToString()),
-                    EmployingDate = Convert.ToDateTime(values.GetValue(i * 6)),
-                    Image = values.GetValue(i * 7).ToString()
+                    Id = Convert.ToInt32(values.GetValue(0 + i)),
+                    FirstName = values.GetValue(1 + i).ToString(),
+                    LastName = values.GetValue(i + 2).ToString(),
+                    ProfessionName = values.GetValue(i + 3).ToString(),
+                    StartHour = TimeSpan.Parse(values.GetValue(i + 4).ToString()),
+                    EndHour = TimeSpan.Parse(values.GetValue(i + 5).ToString()),
+                    EmployingDate = Convert.ToDateTime(values.GetValue(i + 6).ToString()),
+                    Image = values.GetValue(i + 7).ToString()
                 };
                 list.Add(doctor);
-            }
 
+            }
             _dbContext.Dispose();
             return list;
         }
