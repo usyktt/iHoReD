1abd53ded84e021ae69fc3c8996bef316f617d1e
usyktt <32979014+usyktt@users.noreply.github.com>
2018-04-05 13:58:21 +0300

    add Response.flush

--- backend/HoReD/Global.asax.cs
+++ backend/HoReD/Global.asax.cs
@@ -19,6 +19,13 @@ namespace HoReD
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest()
+{
+  if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+  {
+    Response.Flush();
+  }
+}
       
     }
 }
