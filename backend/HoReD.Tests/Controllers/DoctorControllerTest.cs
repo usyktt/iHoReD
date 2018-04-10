8c62a9b27780cd0727f9610ba62e46b80bfa2eb9
ira-halenok <iryna.halenok@gmail.com>
2018-04-10 22:09:27 +0300

    Changed GetDoctors_Count test by using Moq

--- backend/HoReD.Tests/Controllers/DoctorControllerTest.cs
+++ backend/HoReD.Tests/Controllers/DoctorControllerTest.cs
@@ -2,8 +2,10 @@
 using System.Text;
 using System.Collections.Generic;
 using HoReD.Controllers;
+using Entities;
 using Entities.Services;
 using NUnit.Framework;
+using Moq;
 
 namespace HoReDTests.Controllers
 {
@@ -13,14 +15,48 @@ namespace HoReDTests.Controllers
     [TestFixture]
     public class DoctorControllerTest
     {
+        
+        public List<DoctorInfo> GetDoctors()
+        {
+            var list = new List<DoctorInfo>();
+            var doctor1 = new DoctorInfo
+            {
+                Id = 1,
+                FirstName = "name1",
+                LastName = "name2",
+                ProfessionName = "therapist",
+                EmployingDate = new DateTime(2018, 12, 25, 10, 30, 50),
+                StartHour = new TimeSpan(5, 15, 15),
+                EndHour = new TimeSpan(5, 15, 15),
+                Image = "1.jpg"
+            };
+            var doctor2 = new DoctorInfo
+            {
+                Id = 2,
+                FirstName = "name3",
+                LastName = "name4",
+                ProfessionName = "therapist",
+                EmployingDate = new DateTime(2012, 12, 25, 10, 30, 50),
+                StartHour = new TimeSpan(5, 15, 15),
+                EndHour = new TimeSpan(5, 15, 15),
+                Image = "2.jpg"
+            };
+            list.Add(doctor1);
+            list.Add(doctor2);
+            return list;
+        }
 
         [Test]
         public void GetDoctors_Count()
         {
             // Arrange
-            var testData = new FakeDoctorService();
-            var controller = new DoctorController(testData);
+            /*var testData = new FakeDoctorService();
+            var controller = new DoctorController(testData); */
 
+            var mock = new Mock<IDoctorService>();
+            mock.Setup(repo => repo.GetDoctors()).Returns(GetDoctors());
+
+            var controller = new DoctorController(mock.Object);
             // Act
             var response = controller.GetDoctors();
 
