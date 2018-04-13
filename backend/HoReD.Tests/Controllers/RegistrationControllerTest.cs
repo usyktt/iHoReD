986266693769e068cfd45ba6ccf1089f307d7a33
ira-halenok <iryna.halenok@gmail.com>
2018-04-13 16:52:32 +0300

    Added forgotten 'string'

--- backend/HoReD.Tests/Controllers/RegistrationControllerTest.cs
+++ backend/HoReD.Tests/Controllers/RegistrationControllerTest.cs
@@ -6,15 +6,44 @@ using Entities;
 using Entities.Services;
 using NUnit.Framework;
 using Moq;
+using HoReD;
+using HoReD.Models;
 
 namespace HoReD.Tests.Controllers
 {
+
     [TestFixture]
     public class RegistrationControllerTest
     {
-        [Test]
-        public void TestMethod1()
+        Mock<IUserService> mock;
+
+        [SetUp]
+        public void SetUp()
         {
+            mock = new Mock<IUserService>();
+        }
+        [Test]
+        public void CreateNewUser_ReturnOK()
+        {
+            /*bool stat = false;
+            var fake_list = GetFakeBindingModel();
+            mock.Setup(repo => repo.CreateNewUser(stat)).Returns(fake_list);
+            var controller = new DoctorController(mock.Object);
+
+            // Act
+            var result = controller.GetProfessions(stat);*/
+        }
+
+        public RegistrationBindingModel GetFakeBindingModel()
+        {
+            var testParameter = new RegistrationBindingModel()
+            {
+                FirstName = "testFirstname",
+                LastName = "testLastname",
+                Email = "testEmail",
+                Password = "testPassword"
+            };
+            return testParameter;
         }
     }
 }
