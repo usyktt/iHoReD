e7fe8108a252d5ec25d684ee4d86f3108e3dc19c
ira-halenok <iryna.halenok@gmail.com>
2018-04-14 01:10:11 +0300

    Added class for hashing passwords

---
+++ backend/Entities/Hashing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace Entities
+{
+    class Hashing
+    {
+        public static string GenerateSalt()
+        {
+            int workFactor = 12;
+            return BCrypt.Net.BCrypt.GenerateSalt(workFactor);
+        }
+
+        public static string HashingPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GenerateSalt());
+        }
+
+        public static bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
--- backend/Entities/Services/UserService.cs
+++ backend/Entities/Services/UserService.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BCrypt.Net;
 
 namespace Entities.Services
 {
@@ -18,12 +19,14 @@ namespace Entities.Services
 
         public void StoringInfoAboutNewUser(string firstname, string lastname, string email, string password)
         {
+            var password_hash = Hashing.HashingPassword(password);
+
             var regInfo = new Dictionary<string, string>()
             {
                 { "FIRSTNAME", firstname},
                 { "LASTNAME", lastname},
                 { "EMAIL", email},
-                { "PASSWORD", password},
+                { "PASSWORD", password_hash},
             };
             var cmd = "REGISTER_USER";
 
