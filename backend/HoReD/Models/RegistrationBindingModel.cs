86030d83fbe61f6abcc142a7e212bda2731b6978
olegRakus <rakusoleg@gmail.com>
2018-04-13 10:52:32 +0300

    Added validation for Firstname, Lastname, Email and Password

--- backend/HoReD/Models/RegistrationBindingModel.cs
+++ backend/HoReD/Models/RegistrationBindingModel.cs
@@ -8,11 +8,21 @@ namespace HoReD.Models
 {
     public class RegistrationBindingModel
     {
+        [Required]
+        [RegularExpression(@"[A-Za-z]{2,30}")]
         public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z]{2,30}")]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
         public string Email { get; set; }
 
-        [DataType(DataType.Password)]
+        [Required]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,30}$")]
         public string Password { get; set; }
 
     }
