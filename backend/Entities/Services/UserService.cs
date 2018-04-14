ed2848a683eba4b3f1d1df6f06fc0896d5f18ce6
ira-halenok <iryna.halenok@gmail.com>
2018-04-14 18:08:10 +0300

    Created LoginController and LoginUserBindingModel

--- backend/Entities/Services/UserService.cs
+++ backend/Entities/Services/UserService.cs
@@ -32,5 +32,30 @@ namespace Entities.Services
 
             _dbContext.InsertNewUser(cmd, regInfo);
         }
+
+        public User GetUserInfo(string login, string password)
+        {
+            const string cmd = "GET_USER_INFO_START_PAGE";
+            var param = new Dictionary<string, object>()
+            {
+                {"@LOGIN", login},
+                {"@PASSWORD", password}
+            };
+            var str = _dbContext.ExecuteSqlQuery(cmd, '*', param);
+            var values = str.Split('*');
+            var user = new User
+                {
+                    Id = Convert.ToInt32(values.GetValue(0)),
+                    FirstName = values.GetValue(1).ToString(),
+                    LastName = values.GetValue(2).ToString(),
+                    RoleName = values.GetValue(3).ToString(),
+                    Login = values.GetValue(4).ToString(),
+                    Password = values.GetValue(5).ToString(),
+                    Email = values.GetValue(3).ToString(),
+                };
+
+            _dbContext.Dispose();
+            return user;
+        }
     }
 }
---
+++ backend/HoReD/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Entities;
+using Entities.Services;
+using HoReD.Models;
+
+namespace HoReD.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private readonly IDbContext _dbContext = new DbContext();
+
+        //Adds to system inforation about new user
+        [Route("LoginUser")]
+        [HttpPost]
+        [HttpGet]
+        [AllowAnonymous]
+        
+        public IHttpActionResult LoginUser(LoginUserBindingModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Conflict();
+                }
+
+                IUserService user = new UserService(_dbContext);
+                User currentUser = new User();
+                currentUser = user.GetUserInfo("izhydchukdoc", "hgKJ7&hjv*");
+                return Ok(currentUser);
+            }
+            catch (Exception)
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
